3f489483deb43168d84b1c1a6aa6061f01ada5d7
vincent <likonion@outlook.com>
2017-08-04 14:12:38 +0800

    Default

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',
     // assetsPublicPath: './',//改为相对路径以便本地访问
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
